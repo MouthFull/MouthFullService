186d81fbd0fbe8d92262d138dc532dd0edd7d500
Gledinburgh <gledinburgh@gmail.com>
2021-05-19 19:17:19 -0500

    Created Object Models for Recipie, Ingredient, and User

---
+++ MouthFull.Domain/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouthFull.Domain.Models
+{
+    public class User
+    {
+        public List<Recipe> History { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
