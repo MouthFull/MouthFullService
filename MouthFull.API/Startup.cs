3de8647e339c9c7293db0af8d831819d8b857f06
Gledinburgh <gledinburgh@gmail.com>
2021-05-25 20:46:11 -0500

    adding context to History controller

--- MouthFull.API/Startup.cs
+++ MouthFull.API/Startup.cs
@@ -9,53 +9,53 @@ using MouthFull.Storage;
 
 namespace MouthFull.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddCors(options
-                => options.AddDefaultPolicy(builder
-                => builder.AllowAnyOrigin())
-                );
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MouthFull.API", Version = "v1" });
-            });
-            services.AddHttpClient();
-
-            services.AddDbContextFactory<MouthFullContext>(options => 
-            options.UseSqlServer(Configuration.GetConnectionString("mssql")
-            ));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MouthFull.API v1"));
-            }
-
-            app.UseRouting();
-            app.UseCors();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddScoped<MouthFullContext>();
+      services.AddControllers();
+      services.AddCors(options
+          => options.AddDefaultPolicy(builder
+          => builder.AllowAnyOrigin())
+          );
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "MouthFull.API", Version = "v1" });
+      });
+      services.AddHttpClient();
+
+      services.AddDbContextFactory<MouthFullContext>(options =>
+      options.UseSqlServer(Configuration.GetConnectionString("mssql")
+      ));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MouthFull.API v1"));
+      }
+
+      app.UseRouting();
+      app.UseCors();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
